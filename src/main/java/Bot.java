9a7576d699381e80e1dbb06a1ff1a73edccf291f
Nevermind <1276411@bsu.edu.ru>
2021-04-08 01:54:42 +0300

    ВОТ ЭТО БООООТ
    ВОТ ЭТА БЛИН БОТ ВСЕМ БОТАМ БОТ Е МАЕ

---
+++ src/main/java/Bot.java
@@ -0,0 +1,37 @@
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.util.logging.Level;
+
+public class Bot extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        String message = update.getMessage().getText();
+        sendMsg(update.getMessage().getChatId().toString(), message);
+    }
+
+    public synchronized void sendMsg(String chatId, String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(chatId);
+        sendMessage.setText(s);
+        try {
+            sendMessage(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "solar_system_BOT";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1702184934:AAE0NsCeI7vd3aYAPwgsRbI4xuaKkmwKZ8U";
+    }
+}
